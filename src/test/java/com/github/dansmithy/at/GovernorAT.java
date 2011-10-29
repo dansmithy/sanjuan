d54983e5b0d73c5df87cfbc60b10c99d2104c82d
Danny Smith <dansmith1979@gmail.com>
2011-10-29 22:40:45 +0100

    Test cannot choose role when not turn.

--- src/test/java/com/github/dansmithy/at/GovernorAT.java
+++ src/test/java/com/github/dansmithy/at/GovernorAT.java
@@ -8,7 +8,9 @@ import static com.github.dansmithy.driver.BddPartProvider.gameOwnedByJoinedBy;
 import static com.github.dansmithy.driver.BddPartProvider.gameStartedBy;
 import static com.github.dansmithy.driver.BddPartProvider.roleChosenBy;
 import static com.github.dansmithy.driver.BddPartProvider.userExistsAndAuthenticated;
+import static com.github.dansmithy.driver.BddPartProvider.verifyResponseCodeIs;
 import static com.github.dansmithy.driver.BddPartProvider.verifySuccessfulResponseContains;
+import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
 
 import org.junit.Test;
 
@@ -36,6 +38,23 @@ public class GovernorAT {
 						"role : BUILDER")),
 
 				then(verifySuccessfulResponseContains("{ 'state' : 'PLAYING', 'players^name' : [ { 'name' : '#alice', victoryPoints: 1 }, { 'name' : '#bob', victoryPoints: 1 } ], 'roundNumber' : 1, 'rounds^state' : [ { 'state' : 'PLAYING', phases^state : [ { 'state' : 'PLAYING', plays : [ { 'state' : 'AWAITING_INPUT' } ] } ] } ] }")));
+	}
+
+	@Test
+	public void testCanChooseRoleWhenNotYourTurn() {
+
+		bdd.runTest(
+
+				given(userExistsAndAuthenticated("#alice"))
+						.and(userExistsAndAuthenticated("#bob"))
+						.and(gameCreatedBy("#alice"))
+						.and(gameOwnedByJoinedBy("#alice", "#bob"))
+						.and(gameStartedBy("#alice")),
+
+				when(roleChosenBy("#bob", "round : 1; phase : 1",
+						"role : BUILDER")),
+
+				then(verifyResponseCodeIs(HTTP_UNAUTHORIZED)));
 	}
 
 }
