c02208c85aab1f0a6d2f4e561b47dd4372181175
Danny Smith <dansmith1979@gmail.com>
2012-03-25 23:29:46 +0100

    Making it easier to work with properties

--- src/main/java/com/github/dansmithy/sanjuan/twitter/service/impl/EnvironmentVariableConfigurationStore.java
+++ src/main/java/com/github/dansmithy/sanjuan/twitter/service/impl/EnvironmentVariableConfigurationStore.java
@@ -1,18 +1,17 @@
 package com.github.dansmithy.sanjuan.twitter.service.impl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
-
-import javax.inject.Named;
-
 import com.github.dansmithy.sanjuan.twitter.service.ConfigurationStore;
-import com.google.common.base.Objects;
-import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 @Named
 public class EnvironmentVariableConfigurationStore implements ConfigurationStore {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvironmentVariableConfigurationStore.class);
 
     private static final String CONSUMER_KEY_ENVIRONMENT_KEY = "twitter_consumer_key";
 	private static final String CONSUMER_SECRET_ENVIRONMENT_KEY = "twitter_consumer_secret";
@@ -22,81 +21,80 @@ public class EnvironmentVariableConfigurationStore implements ConfigurationStore
     private static final String ACCESS_SECRET_ENVIRONMENT_KEY = "twitter_access_secret";
 	private static final String SAN_JUAN_BASE_URL_ENVIRONMENT_KEY = "base_url";
 
-    private static final String SUPPRESS_EXCEPTIONS_SYSTEM_KEY = "suppress_missing_value_exceptions";
+    private final String consumerKey;
+    private final String consumerSecret;
+    private final String adminConsumerKey;
+    private final String adminConsumerSecret;
+    private final String accessToken;
+    private final String accessSecret;
+    private final String sanJuanBaseUrl;
 
-	private static final String DEFAULT_BASE_URL = "http://localhost:8086";
-    private static final String DEFAULT_DUMMY_VALUE = "empty value";
+    @Inject
+    public EnvironmentVariableConfigurationStore(@Value("${" + CONSUMER_KEY_ENVIRONMENT_KEY + "}") String consumerKey,
+                                                 @Value("${" + CONSUMER_SECRET_ENVIRONMENT_KEY + "}") String consumerSecret,
+                                                 @Value("${" + ADMIN_CONSUMER_KEY_ENVIRONMENT_KEY + "}") String adminConsumerKey,
+                                                 @Value("${" + ADMIN_CONSUMER_SECRET_ENVIRONMENT_KEY + "}") String adminConsumerSecret,
+                                                 @Value("${" + ACCESS_TOKEN_ENVIRONMENT_KEY + "}") String accessToken,
+                                                 @Value("${" + ACCESS_SECRET_ENVIRONMENT_KEY + "}") String accessSecret,
+                                                 @Value("${" + SAN_JUAN_BASE_URL_ENVIRONMENT_KEY + "}") String sanJuanBaseUrl
+                                                 ) {
 
-
-    @Override
-    public String getSanJuanBaseUrl() {
-        return getConfiguredValue(SAN_JUAN_BASE_URL_ENVIRONMENT_KEY, DEFAULT_BASE_URL);
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+        this.adminConsumerKey = adminConsumerKey;
+        this.adminConsumerSecret = adminConsumerSecret;
+        this.accessToken = accessToken;
+        this.accessSecret = accessSecret;
+        this.sanJuanBaseUrl = sanJuanBaseUrl;
+        
+        LOGGER.info(buildPropertyOutput());
     }
 
-    @Override
     public String getConsumerKey() {
-        return getConfiguredValueUsingFilesystemDefault(CONSUMER_KEY_ENVIRONMENT_KEY);
+        return consumerKey;
     }
 
-    /* (non-Javadoc)
-      * @see com.github.dansmithy.twitterlogin.service.impl.ConfigurationStore#getConsumerSecret()
-      */
-	@Override
-	public String getConsumerSecret() {
-        return getConfiguredValueUsingFilesystemDefault(CONSUMER_SECRET_ENVIRONMENT_KEY);
-	}
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
 
-    @Override
     public String getAdminConsumerKey() {
-        return getConfiguredValueUsingFilesystemDefault(ADMIN_CONSUMER_KEY_ENVIRONMENT_KEY);
+        return adminConsumerKey;
     }
 
-    @Override
     public String getAdminConsumerSecret() {
-        return getConfiguredValueUsingFilesystemDefault(ADMIN_CONSUMER_SECRET_ENVIRONMENT_KEY);
+        return adminConsumerSecret;
     }
 
-    @Override
     public String getAccessToken() {
-        return getConfiguredValueUsingFilesystemDefault(ACCESS_TOKEN_ENVIRONMENT_KEY);
+        return accessToken;
     }
 
-    @Override
     public String getAccessSecret() {
-        return getConfiguredValueUsingFilesystemDefault(ACCESS_SECRET_ENVIRONMENT_KEY);
+        return accessSecret;
     }
 
-    private String getConfiguredValueUsingFilesystemDefault(String key) {
-        return getConfiguredValue(key, getFilesystemValue(key));
+    public String getSanJuanBaseUrl() {
+        return sanJuanBaseUrl;
     }
 
-    private String getConfiguredValue(String key, String defaultValue) {
-        String environmentValue = System.getenv(key);
-        try {
-            return Objects.firstNonNull(environmentValue, defaultValue);
-        } catch (NullPointerException e) {
-            if (shouldThrowExceptionsOnMissingValues()) {
-                throw new RuntimeException(String.format("Cannot find value for [%s] as environment variable or otherwise", key), e);
-            } else {
-                return DEFAULT_DUMMY_VALUE;
-            }
-        }
+    private String buildPropertyOutput() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Running with: \n");
+        builder.append(createProperty(CONSUMER_KEY_ENVIRONMENT_KEY, this.consumerKey)).append("\n");
+        builder.append(createProperty(CONSUMER_SECRET_ENVIRONMENT_KEY, this.consumerSecret)).append("\n");
+        builder.append(createProperty(ADMIN_CONSUMER_KEY_ENVIRONMENT_KEY, this.adminConsumerKey)).append("\n");
+        builder.append(createProperty(ADMIN_CONSUMER_SECRET_ENVIRONMENT_KEY, this.adminConsumerSecret)).append("\n");
+        builder.append(createProperty(ACCESS_TOKEN_ENVIRONMENT_KEY, this.accessToken)).append("\n");
+        builder.append(createProperty(ACCESS_SECRET_ENVIRONMENT_KEY, this.accessSecret)).append("\n");
+        builder.append(createProperty(SAN_JUAN_BASE_URL_ENVIRONMENT_KEY, this.sanJuanBaseUrl)).append("\n");
+        return builder.toString();
     }
 
-    private String getFilesystemValue(String key) {
-		String userDir = System.getProperty("user.home");
-		String userFile = userDir + "/sanjuan.secrets.properties";
-        Properties properties = new Properties();
-        try {
-            properties.load(new FileInputStream(new File(userFile)));
-        } catch (IOException e) {
-            return null;
-        }
-        return properties.getProperty(key);
-	}
-
-    private boolean shouldThrowExceptionsOnMissingValues() {
-        return StringUtils.isEmpty(System.getProperty(SUPPRESS_EXCEPTIONS_SYSTEM_KEY));
+    private String createProperty(String key, String value) {
+        return String.format("[%s] = [%s]", key, value);
     }
+
+
 
 }
